a0217f23fef5c29a1ce6cb10f9abe06c47249417
Tomas <developer@Tomass-MacBook-Pro.local>
2019-08-07 07:04:39 -0400

    microservice communication

---
+++ grpc-java-gradle-basic/src/main/java/com/github/tomj0101/grpc/HelloWorldClient.java
@@ -0,0 +1,60 @@
+package com.github.tomj0101.grpc;
+
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorldClient {
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final ManagedChannel channel;
+    private GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public HelloWorldClient(String hostname, int port) {
+        channel = ManagedChannelBuilder.forAddress(hostname, port)
+                .usePlaintext(true)
+                .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void greet(String name) {
+        logger.info("Trying to greet " + name);
+        try {
+            Map<String, String> bagOfTricks = new HashMap<>();
+            bagOfTricks.put("NET", "Netflix");
+            bagOfTricks.put("E", "Eat");
+
+            HelloRequest request = HelloRequest.newBuilder()
+                    .setName(name)
+                    .setAge(900)
+                    .setSentiment(Sentiment.HAPPY)
+                    .build();
+            HelloResponse response = blockingStub.sayHello(request);
+            logger.info("Response: " + response.getMessage());
+        } catch (RuntimeException e) {
+            logger.log(Level.WARNING, "Request to grpc server failed", e);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 42420);
+        String name = args.length > 0 ? args[0] : "unknown";
+
+        try {
+            client.greet(name);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
